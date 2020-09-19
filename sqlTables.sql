035e6425565bb2122a71cad99a0ff062f5a1d205
Gustavo Carvalho <gustavo1net@hotmail.com>
2020-09-19 12:43:15 -0300

    Tabela  SQL

---
+++ sqlTables.sql
@@ -0,0 +1,17 @@
+use  my_db;
+
+create table users(
+	id INT primary key auto_increment not null,
+    name VARCHAR(255) not null,
+    email VARCHAR(255) not null,
+    pass VARCHAR(255) not null,
+    data_criacao TIMESTAMP default now()
+);
+
+create  table tasks(
+	id INT primary key auto_increment not null,
+    id_user INT NOT  NULL,
+    task VARCHAR(255),
+    data_criacao TIMESTAMP default now(),
+    data_conclusao TIMESTAMP default "2000-01-01"
+);
